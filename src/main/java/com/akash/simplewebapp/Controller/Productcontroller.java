44cf01ac605051d5e3155be3bffffac891742b13
akash <akashkavaliakashkavali@gmail.com>
2025-11-14 23:09:50 +0530

    first commit

--- src/main/java/com/akash/simplewebapp/Controller/Productcontroller.java
+++ src/main/java/com/akash/simplewebapp/Controller/Productcontroller.java
@@ -3,8 +3,7 @@ package com.akash.simplewebapp.Controller;
 import com.akash.simplewebapp.Model.Product;
 import com.akash.simplewebapp.Service.Productservice;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +17,26 @@ public class Productcontroller {
     @RequestMapping("/products")
     public List<Product> getProducts(){
         return service.getProducts();
+    }
+
+    @RequestMapping("/products/{prodId}")
+    public Product getProductbyId(@PathVariable int prodId){
+        return service.getProductbyId(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod){
+        System.out.println(prod);
+        service.addProduct(prod);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod){
+        service.updateProduct(prod);
+    }
+
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId){
+        service.deleteProduct(prodId);
     }
 }
--- src/main/java/com/akash/simplewebapp/Service/Productservice.java
+++ src/main/java/com/akash/simplewebapp/Service/Productservice.java
@@ -4,19 +4,48 @@ import com.akash.simplewebapp.Model.Product;
 import org.springframework.stereotype.Service;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 @Service
 
 public class Productservice {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product (101, "Samsung", 50000),
             new Product (102, "Vivo", 30000),
-            new Product (103, "redmi", 20000));
+            new Product (103, "redmi", 20000)));
 
 
     public List<Product> getProducts(){
         return products;
+    }
+
+    public Product getProductbyId(int prodId){
+        return products.stream()
+                .filter(p -> p.getProdId() == prodId)
+                .findFirst().orElse(new Product(100, "No item",0));
+    }
+
+    public void addProduct(Product prod){
+        products.add(prod);
+    }
+
+    public void updateProduct(Product prod) {
+        int index=0;
+
+        for(int i=0;i<products.size();i++)
+            if(products.get(i).getProdId() == prod.getProdId())
+                index = i;
+        products.set(index,prod);
+    }
+
+    public void deleteProduct(int prodId) {
+        int index=0;
+
+        for(int i=0;i<products.size();i++)
+            if(products.get(i).getProdId() == prodId)
+                index = i;
+        products.remove(index);
     }
 }
